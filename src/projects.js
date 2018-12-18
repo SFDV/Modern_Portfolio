3bae78a1d3cdccc98994e7e8ae309146c7b4ffe1
SuperFamousDanV <djudev777@live.com>
2018-12-18 12:07:54 -0500

    Created list function

--- src/projects.js
+++ src/projects.js
@@ -1,17 +1,28 @@
 var projects = [
     {
         title: "The Smash Center",
+        image: "./img/smashlogo.jpg",
         description: "An app that allows users to create customizable profiles, and post information about tournaments they plan on holding",
         link: [["https://dv-capstone-client.herokuapp.com/"]]
     }
 ]
 
 window.onload = function() {
+    populateList();
+}
+
+function populateList(){
+    
     var list = document.getElementById("projectList");
 
     var htmlString = "";
 
     for (var i=0; i < projects.length; i++) {
-        
+        htmlString += "<div class= \"project\">\n";
+        htmlString += "<div class= \"imagewrapper\">\n";
+        htmlString += "<img src=\"" + projects[i].image + "\" /> </div>\n<section>\n";
+        htmlString += "<h1>" + projects[i].title + "</h1>\n";
+        htmlString += "<p>" + projects[i].description + "</p>\n";
+
     }
 }
