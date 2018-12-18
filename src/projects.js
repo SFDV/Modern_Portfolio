0461b918abfb7e1b67fd7ff1a71735dc3e515650
SuperFamousDanV <djudev777@live.com>
2018-12-18 11:52:14 -0500

    Progress projects

---
+++ src/projects.js
@@ -0,0 +1,17 @@
+var projects = [
+    {
+        title: "The Smash Center",
+        description: "An app that allows users to create customizable profiles, and post information about tournaments they plan on holding",
+        link: [["https://dv-capstone-client.herokuapp.com/"]]
+    }
+]
+
+window.onload = function() {
+    var list = document.getElementById("projectList");
+
+    var htmlString = "";
+
+    for (var i=0; i < projects.length; i++) {
+        
+    }
+}
