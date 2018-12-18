7bf9168dbb53e678c1c93d72c9a5334c58ee5fbf
SuperFamousDanV <djudev777@live.com>
2018-12-18 12:24:12 -0500

    Project list 1st try

--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ var projects = [
         title: "The Smash Center",
         image: "./img/smashlogo.jpg",
         description: "An app that allows users to create customizable profiles, and post information about tournaments they plan on holding",
-        link: [["https://dv-capstone-client.herokuapp.com/"]]
+        link: [["https://dv-capstone-client.herokuapp.com/", "Visit the Smash Center!"]]
     }
 ]
 
@@ -12,7 +12,7 @@ window.onload = function() {
 }
 
 function populateList(){
-    
+
     var list = document.getElementById("projectList");
 
     var htmlString = "";
@@ -24,5 +24,11 @@ function populateList(){
         htmlString += "<h1>" + projects[i].title + "</h1>\n";
         htmlString += "<p>" + projects[i].description + "</p>\n";
 
+        for (var h=0; h < projects.links.length; h++) {
+            htmlString += "<a href=\"" + projects[i].links[h][0] + "\">" + projects[i].links[h][1] + "</a> ";
+        }
+
+        htmlString += "\n</section>\n<div>"
     }
+    list.innerHTML = htmlString;
 }
